5ddde2965fb0d794e73654961c5eec924dbe70fd
sagar6462 <sagaarpkr@gmail.com>
2020-10-27 13:21:43 +0545

    First Commit 34:08

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from './components/jumbotron/'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron/>
+    </div>
+  );
+}
+
+export default App;
